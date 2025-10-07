90c03782d3fd5235fcbd481b3b7d39a2709a3a9e
mikla521 <micke@cratz.se>
2025-10-07 21:12:57 +0200

    Adding MainViewModel.
    The viewmodel to be used for handling "logic behind".
    Added a reference to CommunityToolkit.Mvvm which contains useful code to make binding/update easier and an easy way to implement commands for buttons.

---
+++ AddressBook.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Data;
+
+namespace AddressBook.WPF.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    #region Observable properties
+    [ObservableProperty] public string titel;
+    #endregion
+
+    #region Constructor
+    public MainViewModel()
+    {
+        Titel = "AdressBok";
+    }
+    #endregion
+}
