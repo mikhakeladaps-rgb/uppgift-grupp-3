9a8357cb16765582833b703f03b9e3bed913e725
mikla521 <micke@cratz.se>
2025-10-13 15:33:56 +0200

    Added NullToBoolConverter

---
+++ AddressBook.WPF/Converters/Converters.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace AddressBook.WPF.Converters;
+
+public class NullToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value != null; // true om SelectedContact inte är null
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
