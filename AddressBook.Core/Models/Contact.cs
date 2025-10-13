bcac520932c138ef8cf6d4f29f5f56a36caf8e28
mikla521 <micke@cratz.se>
2025-10-13 15:36:51 +0200

    Updated Contact model to nullable types

--- AddressBook.Core/Models/Contact.cs
+++ AddressBook.Core/Models/Contact.cs
@@ -3,11 +3,11 @@ namespace AddressBook.Core.Models;
     public class Contact
 {
         public int Id { get; set;  }        
-        public string Name { get; set; }
-        public string Street { get; set; }
-        public string PostalCode { get; set; }
-        public string City { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Street { get; set; }
+        public string? PostalCode { get; set; }
+        public string? City { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
 
     }
